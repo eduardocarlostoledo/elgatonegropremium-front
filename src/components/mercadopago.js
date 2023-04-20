b0c9daa860a2fe8a3ecf570b7a93da7bf575925c
eduardo carlos toledo <113210953+eduardocarlostoledo@users.noreply.github.com>
2023-04-20 19:34:33 -0300

    Update mercadopago.js

--- src/components/mercadopago.js
+++ src/components/mercadopago.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 //public key
-const mercadopago = new MercadoPago(`${process.env.REACT_APP_MERCADOPAGO}`, {
+const mercadopago = new MercadoPago(process.env.REACT_APP_MERCADOPAGO, {
     locale: 'es-AR' // The most common are: 'pt-BR', 'es-AR' and 'en-US'
 });
   
